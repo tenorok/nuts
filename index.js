4a8be8c80741779f63dddf6b3ce59e861bd13e96
tenorok <ttenor@yandex.ru>
2013-01-05 04:16:25 +0400

    Пример выполнения команд MPD из приложения

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var Nut = require('./Nut.js');
 
-new Nut('54.246.111.22');
+new Nut('54.246.111.22', function(server) {
+
+    this.cmd(server, 'listplaylists', function(result) {
+        console.log(result);
+    });
+
+    this.cmd(server, 'find', 'artist', 'Elvis Presley', function(result) {
+        console.log(result[1]);
+    });
+});
